2d005755f6ea00ee5e62e037c2951d1c422b07ba
Adil BENDOULA <adil.bendoula@gmail.com>
2016-05-26 14:56:09 +0200

    Clean asserts

--- src/test/java/fr/omega/app/mdm/gare/cache/TestApplicationCache.java
+++ src/test/java/fr/omega/app/mdm/gare/cache/TestApplicationCache.java
@@ -1,56 +1,56 @@
 package fr.omega.app.mdm.gare.cache;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import fr.omega.app.mdm.gare.data.Position;
 import fr.omega.app.mdm.gare.helper.DataHelper;
 
+import static org.junit.Assert.*;
+
 public class TestApplicationCache {
-	
+
 	/*
-	 * Only for test purpose, this bean must picked from the spring context
-	 * This bean is declared as singleton
+	 * Only for test purpose, this bean must picked from the spring context This
+	 * bean is declared as singleton
 	 */
 	private final static ApplicationCache _cache = new ApplicationCache();
 
 	@Test
 	public void testInit() {
-		// Lazy cache with only three applications 
-		Assert.assertTrue(_cache.mappingCount() == 3);
-		
+		// Lazy cache with only three applications
+		assertTrue(_cache.mappingCount() == 3);
+
 		// Check that mandatory applications exists on the cache
-		Assert.assertTrue(_cache.containsKey(1));
-		Assert.assertTrue(_cache.containsKey(2));
-		Assert.assertTrue(_cache.containsKey(3));
+		assertTrue(_cache.containsKey(1));
+		assertTrue(_cache.containsKey(2));
+		assertTrue(_cache.containsKey(3));
 	}
 
-	
 	@Test
 	public void testGetApplication() {
-		Assert.assertTrue(_cache.get(null) == null);
-		Assert.assertTrue(_cache.get(1).getName().equals("Application 1"));
-		Assert.assertTrue(_cache.get(2).getName().equals("Application 2"));
-		Assert.assertTrue(_cache.get(3).getName().equals("Application 3"));
+		assertTrue(_cache.get(null) == null);
+		assertTrue(_cache.get(1).getName().equals("Application 1"));
+		assertTrue(_cache.get(2).getName().equals("Application 2"));
+		assertTrue(_cache.get(3).getName().equals("Application 3"));
 	}
-	
+
 	@Test
 	public void testPutIntegerApplication() {
 		// 1
-		Assert.assertTrue(_cache.put(null, null) == null);
-		
+		assertTrue(_cache.put(null, null) == null);
+
 		// 2
 		final Integer id = 1;
 		final String name = "Application 4";
 		final String description = "Description Application 4";
 		final Position position = Position.FRONTOFFICE;
-		
+
 		_cache.put(4, DataHelper.createApplicationFrom(id, name, description, position));
-		
-		Assert.assertTrue(_cache.get(4).getId().intValue() == id);
-		Assert.assertTrue(_cache.get(4).getName().equals(name));
-		Assert.assertTrue(_cache.get(4).getDescription().equals(description));
-		Assert.assertTrue(_cache.get(4).getPosition().equals(position));
+
+		assertTrue(_cache.get(4).getId().intValue() == id);
+		assertTrue(_cache.get(4).getName().equals(name));
+		assertTrue(_cache.get(4).getDescription().equals(description));
+		assertTrue(_cache.get(4).getPosition().equals(position));
 	}
 
 }
--- src/test/java/fr/omega/app/mdm/gare/helper/TestDataHelper.java
+++ src/test/java/fr/omega/app/mdm/gare/helper/TestDataHelper.java
@@ -1,6 +1,6 @@
 package fr.omega.app.mdm.gare.helper;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import fr.omega.app.mdm.gare.data.Application;
@@ -10,12 +10,12 @@ public class TestDataHelper {
 
 	@Test
 	public void testCreateApplicationFromNullId() {
-		
+
 		try {
 			@SuppressWarnings("unused")
 			final Application app = DataHelper.createApplicationFrom(null, null, null, null);
 		} catch (Exception e) {
-			Assert.assertTrue(e instanceof IllegalArgumentException);
+			assertTrue(e instanceof IllegalArgumentException);
 		}
 	}
 
@@ -25,23 +25,22 @@ public class TestDataHelper {
 			@SuppressWarnings("unused")
 			final Application app = DataHelper.createApplicationFrom(1, null, null, null);
 		} catch (Exception e) {
-			Assert.assertTrue(e instanceof IllegalArgumentException);
+			assertTrue(e instanceof IllegalArgumentException);
 		}
 	}
-	
-	
+
 	@Test
 	public void testCreateApplicationFromGoodParams() {
 		final Integer id = 1;
-		final String name = "gare" ;
-		final String description = "gare description" ;
-		final Position position = Position.BACKOFFICE ;
-		
+		final String name = "gare";
+		final String description = "gare description";
+		final Position position = Position.BACKOFFICE;
+
 		final Application app = DataHelper.createApplicationFrom(id, name, description, position);
-		
-		Assert.assertTrue(app.getId().intValue() == id.intValue());
-		Assert.assertTrue(app.getName().equals(name));
-		Assert.assertTrue(app.getDescription().equals(description));
-		Assert.assertTrue(app.getPosition() == Position.BACKOFFICE);
+
+		assertTrue(app.getId().intValue() == id.intValue());
+		assertTrue(app.getName().equals(name));
+		assertTrue(app.getDescription().equals(description));
+		assertTrue(app.getPosition() == Position.BACKOFFICE);
 	}
 }
